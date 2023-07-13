a848c2894503af13eea14358a74f28a7553b13a6
Mateus-PC <mateuswenzler@hotmail.com>
2023-07-13 11:19:25 -0300

    Fazendo uma calculadora básica em C#

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Calculator
+{
+    
+    class Program
+    {
+    
+        static void Main(string[] args)
+        {
+            Divisao();
+
+        }
+
+        static void Soma(){
+
+            Console.Clear();
+
+            Console.WriteLine("Primeiro valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            float resultado = v1 + v2;
+            //Console.WriteLine("O resultado da soma é " + resultado);
+            Console.WriteLine($"O resultado da soma é {resultado}");
+            //Console.WriteLine($"O resultado da soma é {v1 + v2}");
+            //Console.WriteLine($"O resultado da soma é " + (v1 + v2));
+
+            Console.ReadKey();
+        }
+   
+        static void Subtracao(){
+            Console.Clear();
+
+            Console.WriteLine("Primeiro valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            float resultado = v1 - v2;
+            Console.WriteLine($"O resultado da subtração é {resultado}");
+            Console.ReadKey();
+        }
+    
+        static void Divisao(){
+            Console.Clear();
+
+            Console.WriteLine("Primeiro valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");        
+
+            float resultado = v1 / v2;
+            Console.WriteLine($"O resultado da divisão é {resultado}");
+            Console.ReadKey();
+
+        }
+    }
+}
